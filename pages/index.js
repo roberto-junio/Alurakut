4e8ffb3f9018097718a6fe1a96d07d8c658fb841
Roberto Oliveira <roberto.jnio@outlook.com>
2021-07-22 09:40:28 -0300

    Monta layout inicial do Alurakut

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,69 @@
 import styled from 'styled-components'
+import Box from '../src/components/Box'
+import MainGrid from '../src/components/MainGrid'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
 
-export default function Home() {
-  return <Title>My page</Title>
+function ProfileSidebar(props) {
+  console.log(props);
+  return (
+    <Box>
+      <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }}></img>
+    </Box>
+  )
 }
+
+export default function Home() {
+  const githubUser = 'roberto-junio';
+  const pessoasFavoritas = [
+    'WallysonGalvao'
+  ];
+
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSidebar githubUser={githubUser} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            <h1 className='Title' >
+              Bem vindo(a)
+            </h1>
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className='subTitle' >
+              Pessoas ({pessoasFavoritas.length})
+            </h2>
+            <ul>
+              {pessoasFavoritas.map((itemAtual) => {
+                return (
+                  <li>
+                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                      <img src={`https://github.com/${itemAtual}.png`} />
+                      <span>{itemAtual}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+          <Box>
+            <h2 className='subTitle' >
+              Comunidades {/*({comunidadesFavoritas.lenght})*/}
+            </h2>
+          </Box>
+        </div>
+      </MainGrid>
+    </>
+  )
+};
